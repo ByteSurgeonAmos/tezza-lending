739a79b60301dd5955c5aaebe4a9bebba7912b17
ByteSurgeonAmos <wachiraa@co-opbank.co.ke>
2026-09-07 22:04:17 +0300

    feat: bootstrap Spring Modulith project scaffold

---
+++ src/test/java/com/tezza/lending/ModularityTest.java
@@ -0,0 +1,20 @@
+package com.tezza.lending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
+
+class ModularityTest {
+
+    ApplicationModules modules = ApplicationModules.of(LendingApplication.class);
+
+    @Test
+    void verifiesModularStructure() {
+        modules.verify();
+    }
+
+    @Test
+    void createModuleDocumentation() {
+        new Documenter(modules).writeDocumentation();
+    }
+}
